c19e23133c3274fc09682eaf5179d24ffedbaf60
saikat <saikatchakrabortty2@gmail.com>
2019-01-18 14:20:46 +0530

    slight update in movies schema

--- models/Movies.js
+++ models/Movies.js
@@ -1,55 +1,63 @@
 const mongoose = require("mongoose");
-const showtimeSchema = new mongoose.Schema({
+const showtimeSchema = new mongoose.Schema(
+  {
     date: {
-        type:Date
+      type: Date
     },
-    theaters:[{
+    theaters: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref:'Theaters'  
-    }]
+        ref: "Theaters"
+      }
+    ]
+  },
+  {
+    _id: false
+  }
+);
 
-  },{
-      _id:false
-  });
-
-const ratingSchema = new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'EmailList'
+const ratingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "EmailList"
     },
-    created:{
-        type:Date,
-        default:Date.now()
+    created: {
+      type: Date,
+      default: Date.now()
     },
-    rating:{
-        type:Number
+    rating: {
+      type: Number
     }
-},{
-    _id:false
-});
-
+  },
+  {
+    _id: false
+  }
+);
 
 const MovieSchema = new mongoose.Schema({
-   name:{
-    type:String,
-    unique:true
-   },
-   releaseDate:{
-    type:Date,
-   },
-   metadata:{
-       type:Object
-   },
-   ratings:{
-       totalRating:{
-           type:Number
-       },
-       totalCount:{
-           type:Number
-       },
-       allRatings:[ratingSchema]
-   },
-   showTime:[showtimeSchema]
+  name: {
+    type: String,
+    unique: true
+  },
+  releaseDate: {
+    type: Date
+  },
+  ratings: {
+    totalRating: {
+      type: Number
+    },
+    totalCount: {
+      type: Number
+    },
+    allRatings: [ratingSchema]
+  },
+  showTime: [showtimeSchema],
+  metadata: {
+    image:String,
+    description:String,
+    //lots of properties related to movie
+  }
 });
 
 const Movies = mongoose.model("Movies", MovieSchema);
